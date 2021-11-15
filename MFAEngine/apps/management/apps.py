cf348f5e4191348c6c3177664684567b085d425c
justicevR <justicedivine87@gmail.com>
2021-11-15 16:22:29 +0300

    more commits

--- MFAEngine/apps/management/apps.py
+++ MFAEngine/apps/management/apps.py
@@ -2,5 +2,7 @@ from django.apps import AppConfig
 
 
 class ManagementConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
+
     name = 'apps.management'
+    verbose_name = 'API MANAGEMENT'
+
